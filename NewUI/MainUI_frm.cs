492cd1945f39ebdb1e77de659339da00212f21b2
suahdad <35173005+suahdad@users.noreply.github.com>
2018-01-12 23:44:08 +0800

    Add files via upload

---
+++ NewUI/MainUI_frm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+
+
+
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class MainUI_frm : Form
+    {
+        public string ProcDBPath;
+        public string BIPath;
+        static void Main()
+        {
+            Application.Run(new MainUI_frm());
+        }
+        public MainUI_frm()
+        {
+
+            InitializeComponent();
+            ProcDBPath = @"C:\Users\Arandov\Desktop\Database5.accdb";
+            BIPath = @"C:\Users\Arandov\Desktop\Billing Invoice 2018";
+        }
+        private void MainUI_frm_Load(object sender, EventArgs e)
+        {
+            dt_lbl.Text = DateTime.Now.ToString("MMMM dd, yyyy");
+
+            //setup the Data Grid Views
+            OleDbConnection oleAccessCon = new OleDbConnection();
+            Setup DGView = new Setup();
+            DGView.OledbCon(true,ProcDBPath, ref oleAccessCon);
+            DGView.datGrViewDS(dataGridView1, ref oleAccessCon, "TotalProcData");
+            oleAccessCon.Close();
+            //close Connection
+
+            //populate datatable with company info
+            DGView.BIsource(BIPath);
+            this.dataGridView2.DataSource = DGView.dtcompany;
+
+
+
+        }
+        private void optionsToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+           
+           Options_frm ldOpt = new Options_frm();
+           ldOpt.Visible = true;
+
+        }
+
+    }
+
+    public class Setup:MainUI_frm
+    {
+
+        public DataTable dtcompany = new DataTable();
+        public void BIsource(string Path)
+         {
+            //creates a datatable describing the relation of the billing invoice transactions
+             string[] arrtemp = Directory.GetDirectories(Path);
+            List<string> compList = new List<string>();
+            List<string> transactlist = new List<string>();
+             foreach (string s in arrtemp)
+             {
+                compList.Add(s.Substring(Path.Length+1));
+             }
+            foreach (string s in compList)
+            {
+                dtcompany.Columns.Add(s);
+                string sPath = Path+s;
+                string[] trtemp = Directory.GetDirectories(sPath);
+                foreach (string t in trtemp)
+                {
+                    transactlist.Add(t.Substring(sPath.Length+1));
+                }
+                foreach (string t in transactlist)
+                {
+                    
+                }
+            }
+            
+         }
+        public void OledbCon(bool access, string Setuppath , ref OleDbConnection acsCon)
+        {
+            //this function opens a connection
+            string acs;
+            if (access == true)
+            {
+                acs = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Setuppath + ";Persist Security Info=False;";
+            }
+            else
+            {
+                acs = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Setuppath + ";Extended Properties='Excel 12.0 Xml;HDR=YES';";
+            }
+            acsCon = new OleDbConnection(acs);
+            acsCon.Open();
+
+
+        }
+        public void datGrViewDS(DataGridView dat, ref OleDbConnection datcon, string dattable)
+        {//this function connects a data source to a datagridview
+            string adapt = "Select * from " + dattable;
+            OleDbDataAdapter datadapt = new OleDbDataAdapter(adapt, datcon);
+            DataTable datdt = new DataTable();
+            datadapt.Fill(datdt);
+            dat.DataSource = datdt;
+        }
+    }
+    public class Operation
+    { }
+
+
+}
---
+++ NewUI/Options_frm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Options_frm
+    {
+       
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Options_frm));
+            this.Options_lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Options_lbl
+            // 
+            this.Options_lbl.AutoSize = true;
+            this.Options_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Options_lbl.Location = new System.Drawing.Point(237, 46);
+            this.Options_lbl.Name = "Options_lbl";
+            this.Options_lbl.Size = new System.Drawing.Size(137, 31);
+            this.Options_lbl.TabIndex = 0;
+            this.Options_lbl.Text = "OPTIONS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source Path";
+            // 
+            // Options_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 481);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Options_lbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Options_frm";
+            this.Text = "Options";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Options_lbl;
+        private System.Windows.Forms.Label label1;
+    }
+}
